39b0c4c8d959effe05ce73c99fd5eb884ee414f6
Harsh Goel <148752140+harshg3010@users.noreply.github.com>
2026-06-03 17:15:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import userRouter from "./routes/user.routes.js";
 import geminiResponse from "./gemini.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://virtualassistant-oium.onrender.com", credentials: true }));
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
